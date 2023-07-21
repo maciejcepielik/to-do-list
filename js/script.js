8b904b0af50e767d0bd2bc52071b37cfe1bce5fe
Maciej Cepielik <maciej.cepielik@gmail.com>
2023-07-21 14:32:28 +0200

    separate function onFormSubmit created

--- js/script.js
+++ js/script.js
@@ -40,23 +40,25 @@
         render();
     };
 
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        // console.log(newTaskContent)
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    };
+
     const init = () => {
         render();
 
         const form = document.querySelector(".js-form");
 
-        form.addEventListener("submit", (event) => {
-            event.preventDefault();
-
-            const newTaskContent = document.querySelector(".js-newTask").value.trim();
-            // console.log(newTaskContent)
-
-            if (newTaskContent === "") {
-                return;
-            }
-
-            addNewTask(newTaskContent);
-        });
+        form.addEventListener("submit", onFormSubmit);
 
     };
 
